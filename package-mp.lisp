231247b44ec5b4de85818efdd445e6901611bd34
Zach Beane <xach@xach.com>
2016-11-18 10:46:47 -0500

    Kill MP's copy of WITH-TIMEOUT.

--- package-mp.lisp
+++ package-mp.lisp
@@ -5,10 +5,6 @@
 
 (in-package #:zacl)
 
-(defmacro mp:with-timeout ((time &body actions) &body body)
-  (declare (ignorable time actions))
-  `(progn ,@body))
-
 (defmacro mp:with-process-lock (lock-form &body body)
   (declare (ignorable lock-form))
   `(progn ,@body))
